7fa044d7663575ff5be584e099afaa3fe8e2e652
a22davidil <a22davidil@192.168.56.1>
2023-01-09 12:44:19 +0100

    prueba comentario

---
+++ src/main/java/com/mycompany/apalabradodia/APalabraDoDia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.apalabradodia;
+
+/**
+ *Comentario de David
+ * @author a22davidil
+ */
+public class APalabraDoDia {
+
+    public static void main(String[] args) {
+        Palabra palabra = new Palabra();
+        palabra.xogar();
+        
+        
+        
+        //mas comentarios
+    }
+}
